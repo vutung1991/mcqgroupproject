af260b89463399f0a1eee638d32664f33535c870
vutung1991 <vutung1991@gmail.com>
2023-10-11 11:45:52 -0400

    add code

---
+++ src/main/java/com/example/mcqgroupbe/McqGroupBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.mcqgroupbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McqGroupBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McqGroupBeApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/mcqgroupbe/entity/Question.java
@@ -0,0 +1,22 @@
+package com.example.mcqgroupbe.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Question {
+
+    private int qNo;
+    private String question;
+    private String opt_a;
+    private String opt_b;
+    private String opt_c;
+    private String opt_d;
+    private int correct;
+    private int setId;
+
+}
